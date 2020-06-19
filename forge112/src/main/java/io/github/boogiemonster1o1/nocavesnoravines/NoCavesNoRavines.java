7f51392a61651ea86bab0a048004a08538ca465b
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-19 08:51:48 +0530

    Added basic mod file
     Changes to be committed:
    	modified:   forge112/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java
    	new file:   forge112/src/main/resources/assets/nocavesnoravines/lang/en_us.lang

--- forge112/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java
+++ forge112/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java
@@ -1,10 +1,14 @@
 package io.github.boogiemonster1o1.nocavesnoravines;
 
 import net.minecraft.init.Blocks;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = NoCavesNoRavines.MODID, name = NoCavesNoRavines.NAME, version = NoCavesNoRavines.VERSION)
@@ -15,14 +19,53 @@ public class NoCavesNoRavines{
 
     private static Logger logger;
 
-    @EventHandler
+    @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
         logger = event.getModLog();
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event){
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @Mod.EventBusSubscriber(modid=MODID)
+    static class ModEventHandler{
+        @SubscribeEvent
+        public static void onConfigChanged(final ConfigChangedEvent.OnConfigChangedEvent event) {
+            if(event.getModID().equals(MODID)){
+                ConfigManager.sync(MODID,Config.Type.INSTANCE);
+            }
+        }
+
+    }
+
+    @Config(modid =MODID)
+    @Config.LangKey("nocavesnoravines.config.title")
+    static class CommonConfiguration{
+        @Comment("Remove underwater caves")
+        public static boolean removeUnderwaterCaves = true;
+
+        @Comment("Remove underwater ravines")
+        public static boolean removeUnderwaterRavines = true;
+
+        @Comment("Remove caves")
+        public static boolean removeCaves = true;
+
+        @Comment("Remove ravines")
+        public static boolean removeRavines = true;
+
+        @Comment("Remove water lakes")
+        public static boolean removeLakes = false;
+
+        @Comment("Remove lava lakes")
+        public static boolean removeLavaLakes = false;
+
+        @Comment("Remove water springs")
+        public static boolean removeSprings = false;
+
+        @Comment("Remove lava springs")
+        public static boolean removeLavaSprings = false;
     }
 }
