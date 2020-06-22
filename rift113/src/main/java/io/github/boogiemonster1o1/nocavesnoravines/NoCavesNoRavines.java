bc0e8980d04a47249e57a5afb3ac146d518c8f5d
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-06-22 13:08:36 +0530

    Created sub project for rift
     Changes to be committed:
    	modified:   build.gradle
    	new file:   rift113/build.gradle
    	new file:   rift113/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java
    	new file:   rift113/src/main/resources/pack.mcmeta
    	new file:   rift113/src/main/resources/pack.png
    	new file:   rift113/src/main/resources/riftmod.json
    	modified:   settings.gradle

---
+++ rift113/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java
@@ -0,0 +1,30 @@
+package io.github.boogiemonster1o1.nocavesnoravines;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dimdev.rift.listener.BootstrapListener;
+import org.dimdev.rift.listener.MinecraftStartListener;
+
+public class NoPVPCooldown implements MinecraftStartListener, BootstrapListener {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "nocooldown";
+    public static final String MOD_NAME = "No PVP Cooldown";
+
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+    @Override
+    public void afterVanillaBootstrap() {
+        log(Level.WARN,"This version of NoCavesNoRavines is made for Rift 1.13.2, so errors may exist. Please report them to the github issue tracker.");
+    }
+
+    @Override
+    public void onMinecraftStart() {
+        log(Level.INFO, "Initializing");
+    }
+}
