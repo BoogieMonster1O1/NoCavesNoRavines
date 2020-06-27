3968791b45c935a9c93b7ddc099a75bd90413512
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-06-27 11:43:12 +0530

    Removed comment
     Changes to be committed:
    	modified:   forge115/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java

--- forge115/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java
+++ forge115/src/main/java/io/github/boogiemonster1o1/nocavesnoravines/NoCavesNoRavines.java
@@ -47,7 +47,6 @@ public class NoCavesNoRavines {
 
     private void setup(final FMLCommonSetupEvent event) {
         DeferredWorkQueue.runLater(this::modifyBiomes);
-//        this.modifyBiomes();
     }
 
     private void modifyBiomes() {
